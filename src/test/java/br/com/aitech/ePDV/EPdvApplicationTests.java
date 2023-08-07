2e207686379bca0d6d2b0ea62b1d12ba12cb3bdc
Verissimo Santos <jose.sacramento@grupoboticario.com.br>
2023-08-07 17:17:06 -0300

    initial commit

---
+++ src/test/java/br/com/aitech/ePDV/EPdvApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.aitech.ePDV;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EPdvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
